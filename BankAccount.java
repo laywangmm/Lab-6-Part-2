8fc723b4086ba8877c2d55f279776f5b25aeeecf
laywangmm <laywangbd@gmail.com>
2025-11-07 10:56:43 -0800

    updates

--- BankAccount.java
+++ BankAccount.java
@@ -8,27 +8,51 @@
  * @version v1.0
  * @since 11/6/2025
  */
- public class BankAccount {
+public class BankAccount {
     private double balance;
     private double deposit;
     private double withdraw;
     private double interest;
     private double monthlyIntRate;
 
+    /**
+     * No-argument constructor.
+     * Sets the monthly interest rate to 0.044 / 12
+     * and initializes all other values to 0.
+     */
     public BankAccount() {
         monthlyIntRate = 0.044 / 12;
     }
 
+    /**
+     * Constructor for whole-dollar starting balances.
+     * Divides annual interest rate by 100, then by 12 to set monthlyIntRate.
+     *
+     * @param inBalance the initial account balance in whole dollars
+     * @param intRate the annual interest rate as a percent (for 4.4%)
+     */
     public BankAccount(int inBalance, double intRate) {
         balance = inBalance;
         monthlyIntRate = (intRate / 100) / 12;
     }
 
+    /**
+     * Constructor for currency starting balances.
+     * Divides annual interest rate by 100, then by 12 to set monthlyIntRate.
+     *
+     * @param inBalance the initial account balance in currency format
+     * @param intRate the annual interest rate as a percent (e.g., 4.4 for 4.4%)
+     */
     public BankAccount(double inBalance, double intRate) {
         balance = inBalance;
         monthlyIntRate = (intRate / 100) / 12;
     }
 
+    /**
+     * Adds an integer deposit to the account.
+     *
+     * @param add the amount to deposit
+     */
     public void makeDeposit(int add) {
         if (add > 0) {
             balance += add;
@@ -36,6 +60,11 @@
         }
     }
 
+    /**
+     * Adds a currency deposit to the account.
+     *
+     * @param add the amount to deposit
+     */
     public void makeDeposit(double add) {
         if (add > 0) {
             balance += add;
@@ -43,6 +72,11 @@
         }
     }
 
+    /**
+     * Subtracts an integer withdrawal from the account.
+     *
+     * @param sub the amount to withdraw
+     */
     public void makeWithdraw(int sub) {
         if (sub > 0) {
             balance -= sub;
@@ -50,6 +84,11 @@
         }
     }
 
+    /**
+     * Subtracts a currency withdrawal from the account.
+     *
+     * @param sub the amount to withdraw
+     */
     public void makeWithdraw(double sub) {
         if (sub > 0) {
             balance -= sub;
@@ -57,6 +96,10 @@
         }
     }
 
+    /**
+     * Calculates and adds monthly interest to the balance.
+     * Interest is only applied if the balance is greater than 0.
+     */
     public void calcInterest() {
         if (balance > 0) {
             double m = balance * monthlyIntRate;
@@ -65,8 +108,39 @@
         }
     }
 
-    public double getBalance() { return balance; }
-    public double getDeposit() { return deposit; }
-    public double getWithdraw() { return withdraw; }
-    public double getInterest() { return interest; }
+    /**
+     * Gets the current balance.
+     *
+     * @return the current account balance
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    /**
+     * Gets the total amount deposited.
+     *
+     * @return the total deposits
+     */
+    public double getDeposit() {
+        return deposit;
+    }
+
+    /**
+     * Gets the total amount withdrawn.
+     *
+     * @return the total withdrawals
+     */
+    public double getWithdraw() {
+        return withdraw;
+    }
+
+    /**
+     * Gets the total interest earned.
+     *
+     * @return the total interest
+     */
+    public double getInterest() {
+        return interest;
+    }
 }
